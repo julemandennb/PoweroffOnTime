cb39e3237e8516c3bb7881942842d18a4dbc7722
julemandennb <bo.naerumhansen@gmail.com>
2023-04-01 19:18:58 +0200

    add radio btn and MessageBox to 5 min and add time

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,113 +28,217 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(form1));
-            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
-            this.contextMenuStripnotifyIcon = new System.Windows.Forms.ContextMenuStrip(this.components);
-            this.timeToShowUser = new System.Windows.Forms.ToolStripTextBox();
-            this.testToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.lukkeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.toolStripMenuItemOpen = new System.Windows.Forms.ToolStripMenuItem();
-            this.TimeToShowOnForm = new System.Windows.Forms.Label();
-            this.button30min = new System.Windows.Forms.Button();
-            this.contextMenuStripnotifyIcon.SuspendLayout();
-            this.SuspendLayout();
+            notifyIcon = new NotifyIcon(components);
+            contextMenuStripnotifyIcon = new ContextMenuStrip(components);
+            timeToShowUser = new ToolStripTextBox();
+            toolStripMenuItemOpen = new ToolStripMenuItem();
+            lukkeToolStripMenuItem = new ToolStripMenuItem();
+            TimeToShowOnForm = new Label();
+            button30min = new Button();
+            button90min = new Button();
+            button60min = new Button();
+            button120min = new Button();
+            button180min = new Button();
+            radioButtonAdd = new RadioButton();
+            radioButtonSetTime = new RadioButton();
+            radioButtonAlertOff = new RadioButton();
+            radioButtonAlertOn = new RadioButton();
+            contextMenuStripnotifyIcon.SuspendLayout();
+            SuspendLayout();
             // 
             // notifyIcon
             // 
-            this.notifyIcon.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
-            this.notifyIcon.ContextMenuStrip = this.contextMenuStripnotifyIcon;
-            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
-            this.notifyIcon.Text = "notifyIcon";
-            this.notifyIcon.Visible = true;
+            notifyIcon.BalloonTipIcon = ToolTipIcon.Info;
+            notifyIcon.ContextMenuStrip = contextMenuStripnotifyIcon;
+            notifyIcon.Icon = (Icon)resources.GetObject("notifyIcon.Icon");
+            notifyIcon.Text = "notifyIcon";
+            notifyIcon.Visible = true;
+            notifyIcon.DoubleClick += notifyIcon_DoubleClick;
             // 
             // contextMenuStripnotifyIcon
             // 
-            this.contextMenuStripnotifyIcon.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.timeToShowUser,
-            this.testToolStripMenuItem,
-            this.lukkeToolStripMenuItem,
-            this.toolStripMenuItemOpen});
-            this.contextMenuStripnotifyIcon.Name = "contextMenuStripnotifyIcon";
-            this.contextMenuStripnotifyIcon.Size = new System.Drawing.Size(161, 95);
+            contextMenuStripnotifyIcon.Items.AddRange(new ToolStripItem[] { timeToShowUser, toolStripMenuItemOpen, lukkeToolStripMenuItem });
+            contextMenuStripnotifyIcon.Name = "contextMenuStripnotifyIcon";
+            contextMenuStripnotifyIcon.Size = new Size(161, 73);
             // 
             // timeToShowUser
             // 
-            this.timeToShowUser.Name = "timeToShowUser";
-            this.timeToShowUser.ReadOnly = true;
-            this.timeToShowUser.Size = new System.Drawing.Size(100, 23);
-            // 
-            // testToolStripMenuItem
-            // 
-            this.testToolStripMenuItem.Name = "testToolStripMenuItem";
-            this.testToolStripMenuItem.Size = new System.Drawing.Size(160, 22);
-            this.testToolStripMenuItem.Text = "test";
-            this.testToolStripMenuItem.Click += new System.EventHandler(this.testToolStripMenuItem_Click);
-            // 
-            // lukkeToolStripMenuItem
-            // 
-            this.lukkeToolStripMenuItem.Name = "lukkeToolStripMenuItem";
-            this.lukkeToolStripMenuItem.Size = new System.Drawing.Size(160, 22);
-            this.lukkeToolStripMenuItem.Text = "Lukke";
-            this.lukkeToolStripMenuItem.Click += new System.EventHandler(this.lukkeToolStripMenuItem_Click);
+            timeToShowUser.Name = "timeToShowUser";
+            timeToShowUser.ReadOnly = true;
+            timeToShowUser.Size = new Size(100, 23);
             // 
             // toolStripMenuItemOpen
             // 
-            this.toolStripMenuItemOpen.Name = "toolStripMenuItemOpen";
-            this.toolStripMenuItemOpen.Size = new System.Drawing.Size(160, 22);
-            this.toolStripMenuItemOpen.Text = "Open";
-            this.toolStripMenuItemOpen.Click += new System.EventHandler(this.toolStripMenuItemOpen_Click);
+            toolStripMenuItemOpen.Name = "toolStripMenuItemOpen";
+            toolStripMenuItemOpen.Size = new Size(160, 22);
+            toolStripMenuItemOpen.Text = "Open";
+            toolStripMenuItemOpen.Click += toolStripMenuItemOpen_Click;
+            // 
+            // lukkeToolStripMenuItem
+            // 
+            lukkeToolStripMenuItem.Name = "lukkeToolStripMenuItem";
+            lukkeToolStripMenuItem.Size = new Size(160, 22);
+            lukkeToolStripMenuItem.Text = "Lukke";
+            lukkeToolStripMenuItem.Click += lukkeToolStripMenuItem_Click;
             // 
             // TimeToShowOnForm
             // 
-            this.TimeToShowOnForm.AutoSize = true;
-            this.TimeToShowOnForm.Font = new System.Drawing.Font("Segoe UI", 99.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.TimeToShowOnForm.ForeColor = System.Drawing.Color.Black;
-            this.TimeToShowOnForm.Location = new System.Drawing.Point(2, 9);
-            this.TimeToShowOnForm.Margin = new System.Windows.Forms.Padding(0);
-            this.TimeToShowOnForm.Name = "TimeToShowOnForm";
-            this.TimeToShowOnForm.Size = new System.Drawing.Size(796, 175);
-            this.TimeToShowOnForm.TabIndex = 1;
-            this.TimeToShowOnForm.Text = "No Time Set";
+            TimeToShowOnForm.AutoSize = true;
+            TimeToShowOnForm.Font = new Font("Segoe UI", 99.75F, FontStyle.Regular, GraphicsUnit.Point);
+            TimeToShowOnForm.ForeColor = Color.Black;
+            TimeToShowOnForm.Location = new Point(2, 9);
+            TimeToShowOnForm.Margin = new Padding(0);
+            TimeToShowOnForm.Name = "TimeToShowOnForm";
+            TimeToShowOnForm.Size = new Size(796, 175);
+            TimeToShowOnForm.TabIndex = 1;
+            TimeToShowOnForm.Text = "No Time Set";
             // 
             // button30min
             // 
-            this.button30min.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.button30min.Location = new System.Drawing.Point(12, 198);
-            this.button30min.Name = "button30min";
-            this.button30min.Size = new System.Drawing.Size(159, 72);
-            this.button30min.TabIndex = 2;
-            this.button30min.Text = "30 min";
-            this.button30min.UseVisualStyleBackColor = true;
-            this.button30min.Click += new System.EventHandler(this.button30min_Click);
+            button30min.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            button30min.Location = new Point(12, 187);
+            button30min.Name = "button30min";
+            button30min.Size = new Size(159, 46);
+            button30min.TabIndex = 2;
+            button30min.Tag = "";
+            button30min.Text = "30 min";
+            button30min.UseVisualStyleBackColor = true;
+            button30min.Click += TimeButton_Click;
+            // 
+            // button90min
+            // 
+            button90min.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            button90min.Location = new Point(12, 287);
+            button90min.Name = "button90min";
+            button90min.Size = new Size(159, 38);
+            button90min.TabIndex = 3;
+            button90min.Text = "90 min";
+            button90min.UseVisualStyleBackColor = true;
+            button90min.Click += TimeButton_Click;
+            // 
+            // button60min
+            // 
+            button60min.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            button60min.Location = new Point(12, 239);
+            button60min.Name = "button60min";
+            button60min.Size = new Size(159, 42);
+            button60min.TabIndex = 4;
+            button60min.Text = "60 min";
+            button60min.UseVisualStyleBackColor = true;
+            button60min.Click += TimeButton_Click;
+            // 
+            // button120min
+            // 
+            button120min.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            button120min.Location = new Point(12, 331);
+            button120min.Name = "button120min";
+            button120min.Size = new Size(159, 38);
+            button120min.TabIndex = 5;
+            button120min.Text = "120 min";
+            button120min.UseVisualStyleBackColor = true;
+            button120min.Click += TimeButton_Click;
+            // 
+            // button180min
+            // 
+            button180min.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            button180min.Location = new Point(12, 375);
+            button180min.Name = "button180min";
+            button180min.Size = new Size(159, 38);
+            button180min.TabIndex = 7;
+            button180min.Text = "180 min";
+            button180min.UseVisualStyleBackColor = true;
+            button180min.Click += TimeButton_Click;
+            // 
+            // radioButtonAdd
+            // 
+            radioButtonAdd.AccessibleName = "radioButtonTime";
+            radioButtonAdd.AutoSize = true;
+            radioButtonAdd.Checked = true;
+            radioButtonAdd.Location = new Point(183, 187);
+            radioButtonAdd.Name = "radioButtonAdd";
+            radioButtonAdd.Size = new Size(76, 19);
+            radioButtonAdd.TabIndex = 8;
+            radioButtonAdd.TabStop = true;
+            radioButtonAdd.Text = "Add Time";
+            radioButtonAdd.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonSetTime
+            // 
+            radioButtonSetTime.AccessibleName = "radioButtonTime";
+            radioButtonSetTime.AutoSize = true;
+            radioButtonSetTime.Location = new Point(265, 187);
+            radioButtonSetTime.Name = "radioButtonSetTime";
+            radioButtonSetTime.Size = new Size(70, 19);
+            radioButtonSetTime.TabIndex = 9;
+            radioButtonSetTime.Text = "Set Time";
+            radioButtonSetTime.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonAlertOff
+            // 
+            radioButtonAlertOff.AccessibleName = "radioButtonAlert";
+            radioButtonAlertOff.AutoSize = true;
+            radioButtonAlertOff.Location = new Point(267, 239);
+            radioButtonAlertOff.Name = "radioButtonAlertOff";
+            radioButtonAlertOff.Size = new Size(68, 19);
+            radioButtonAlertOff.TabIndex = 11;
+            radioButtonAlertOff.Text = "Alert off";
+            radioButtonAlertOff.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonAlertOn
+            // 
+            radioButtonAlertOn.AccessibleName = "radioButtonAlert";
+            radioButtonAlertOn.AutoSize = true;
+            radioButtonAlertOn.Checked = true;
+            radioButtonAlertOn.Location = new Point(185, 239);
+            radioButtonAlertOn.Name = "radioButtonAlertOn";
+            radioButtonAlertOn.Size = new Size(67, 19);
+            radioButtonAlertOn.TabIndex = 10;
+            radioButtonAlertOn.TabStop = true;
+            radioButtonAlertOn.Text = "Alert on";
+            radioButtonAlertOn.UseVisualStyleBackColor = true;
             // 
             // form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button30min);
-            this.Controls.Add(this.TimeToShowOnForm);
-            this.MaximizeBox = false;
-            this.Name = "form1";
-            this.Text = "Form1";
-            this.contextMenuStripnotifyIcon.ResumeLayout(false);
-            this.contextMenuStripnotifyIcon.PerformLayout();
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(radioButtonAlertOff);
+            Controls.Add(radioButtonAlertOn);
+            Controls.Add(radioButtonSetTime);
+            Controls.Add(radioButtonAdd);
+            Controls.Add(button180min);
+            Controls.Add(button120min);
+            Controls.Add(button60min);
+            Controls.Add(button90min);
+            Controls.Add(button30min);
+            Controls.Add(TimeToShowOnForm);
+            MaximizeBox = false;
+            Name = "form1";
+            Text = "Form1";
+            contextMenuStripnotifyIcon.ResumeLayout(false);
+            contextMenuStripnotifyIcon.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private NotifyIcon notifyIcon;
         private ContextMenuStrip contextMenuStripnotifyIcon;
-        private ToolStripMenuItem testToolStripMenuItem;
         private ToolStripTextBox timeToShowUser;
         private ToolStripMenuItem lukkeToolStripMenuItem;
         private ToolStripMenuItem toolStripMenuItemOpen;
         private Label TimeToShowOnForm;
         private Button button30min;
+        private Button button90min;
+        private Button button60min;
+        private Button button120min;
+        private Button button180min;
+        private RadioButton radioButtonAdd;
+        private RadioButton radioButtonSetTime;
+        private RadioButton radioButtonAlertOff;
+        private RadioButton radioButtonAlertOn;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,25 +1,55 @@
+using System.Diagnostics;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
 namespace PoweroffOnTime
 {
     public partial class form1 : Form
     {
 
         private System.Windows.Forms.Timer timer1;
-        private int counter = 60;
+        private int counter = 0;
+        public bool MessageBoxIsOpen = false;
+
+        public bool killThis = false;
 
         public form1()
         {
             InitializeComponent();
-
             timer1 = new System.Windows.Forms.Timer();
             timer1.Tick += new EventHandler(timercountdown_Tick);
             timer1.Interval = 1000;
 
+            //this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.ShowInTaskbar = false;
+
+
+
         }
 
-
-        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        protected override void OnFormClosing(FormClosingEventArgs e)
         {
-            Application.Exit();
+            base.OnFormClosing(e);
+
+            if (e.CloseReason == CloseReason.WindowsShutDown) return;
+
+            if (!this.killThis)
+            {
+                e.Cancel = true;
+
+                notifyIcon.BalloonTipIcon = ToolTipIcon.Info;
+
+                notifyIcon.BalloonTipText = "is not close";
+
+                notifyIcon.BalloonTipTitle = "is run";
+
+                notifyIcon.ShowBalloonTip(1000);
+            }
+
+
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+
+
         }
 
         private void testToolStripMenuItem_Click(object sender, EventArgs e)
@@ -37,6 +67,7 @@ namespace PoweroffOnTime
 
         private void lukkeToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            this.killThis = true;
             this.Close();
         }
 
@@ -45,21 +76,79 @@ namespace PoweroffOnTime
             this.WindowState = FormWindowState.Normal;
         }
 
-        private void button30min_Click(object sender, EventArgs e)
+        private void TimeButton_Click(object sender, EventArgs e)
         {
-            timer1.Stop();
-            timer1.Start();
 
-            counter = 1800;
+            if (sender is Button)
+            {
+                Button button = (Button)sender;
+                timer1.Stop();
+                timer1.Start();
 
-            showTime();
+                int numToAdd = TimeToAdd(button.Text);
+
+                if (this.radioButtonAdd.Checked)
+                {
+                    counter += numToAdd;
+                }
+                else
+                {
+                    counter = numToAdd;
+                }
+
+
+                showTime();
+            }
+        }
+
+        private int TimeToAdd(string text)
+        {
+            int numToAdd = 1800;
+
+            switch (text)
+            {
+                case "180 min":
+                    numToAdd += 1800;
+                    goto case "120 min";
+                case "120 min":
+                    numToAdd += 1800;
+                    goto case "90 min";
+                case "90 min":
+                    numToAdd += 1800;
+                    goto case "60 min";
+                case "60 min":
+                    numToAdd += 1800;
+                    break;
+                default:
+                    break;
+            }
+
+            return numToAdd;
         }
 
         private void timercountdown_Tick(object sender, EventArgs e)
         {
             counter--;
             if (counter <= 0)
+            {
                 timer1.Stop();
+#if !DEBUG
+                var psi = new ProcessStartInfo("shutdown", "/s /t 0");
+                psi.CreateNoWindow = true;
+                psi.UseShellExecute = false;
+                Process.Start(psi);
+#endif
+            }
+            else if (this.radioButtonAlertOn.Checked && counter <= 300 && !this.MessageBoxIsOpen)
+            {
+                this.MessageBoxIsOpen = true;
+                DialogResult dialogResult = MessageBox.Show(new Form { TopMost = true }, "add 30 min", "5 min back", MessageBoxButtons.YesNo);
+                if (dialogResult == DialogResult.Yes)
+                {
+                    counter += 1800;
+                    this.MessageBoxIsOpen = false;
+                }
+            }
 
             showTime();
         }
@@ -67,12 +156,15 @@ namespace PoweroffOnTime
         private void showTime()
         {
             TimeSpan time = TimeSpan.FromSeconds(counter);
-            string timestr =  time.ToString("hh':'mm':'ss");
+            string timestr = time.ToString("hh':'mm':'ss");
 
             TimeToShowOnForm.Text = timestr;
             timeToShowUser.Text = timestr;
         }
 
-
+        private void notifyIcon_DoubleClick(object sender, EventArgs e)
+        {
+            this.WindowState = System.Windows.Forms.FormWindowState.Normal;
+        }
     }
 }
